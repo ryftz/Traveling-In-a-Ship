c2cf4ffba724f853b79a7a24f2d50327016a97f3
ryftz <88239811+ryftz@users.noreply.github.com>
2021-08-06 18:26:56 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+
+function preload(){
+seaImg = loadImage("sea.png");
+shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png", "ship-4.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  sea=createSprite(400,200)
+  sea.addImage(seaImg);
+  sea.velocityX= -7
+  sea.scale= 0.3
+  ship = createSprite(130,200,20,80)
+  ship.addAnimation("sailing",shipImg1)
+  ship.scale = 0.25
+}
+
+function draw() {
+  background("blue");
+  if(sea.x < 0){
+    sea.x = sea.width/8;
+  }
+  drawSprites();
+}
